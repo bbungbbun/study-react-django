afa9207b7c727a1f005ae3b4e9909ca9816133c8
bbungbbun <tnqkrdbsl@naver.com>
2023-10-31 15:38:41 +0900

    serializers 생성

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guset_can_quase', 'votes_to_skip', 'created_at')
+
+    
--- api/urls.py
+++ api/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('home', main),
-    path('', main),
+    path('home', RoomView.as_view()),
 ]
